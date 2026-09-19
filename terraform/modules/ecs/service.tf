c7731ca90400eb6df5e915da96bbefbc5354c4c0
Lawal Sulaiman <lawal2401gmail.com>
2026-09-19 16:19:53 +0000

    Enable ECS automatic deployment rollback

--- terraform/modules/ecs/service.tf
+++ terraform/modules/ecs/service.tf
@@ -53,6 +53,15 @@ resource "aws_ecs_service" "app" {
   desired_count = 1
   launch_type   = "FARGATE"
 
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  health_check_grace_period_seconds  = 60
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
   network_configuration {
     subnets          = var.public_subnet_ids
     security_groups  = [var.ecs_security_group_id]
